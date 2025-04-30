65db7492b903e002c37c694ebe45830886ef8383
Vladislav <whatislov@mail.ru>
2025-05-01 01:08:40 +0300

    feat: add local deploy config and set up xdebug

--- public/index.php
+++ public/index.php
@@ -10,7 +10,6 @@ $container = new ContainerDI(__DIR__ . '/../config/');
 $container->compile();
 
 $router = $container->get(Router::class);
-[$handler, $methodName] = $router->get('/', 'GET');
+[$className, $methodName] = $router->get('/', 'GET');
 
-$instance = $container->get($handler);
-call_user_func_array([$instance, $methodName], ["one"]);
+$instance = $container->get($className);
