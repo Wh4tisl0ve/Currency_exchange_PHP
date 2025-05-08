d3e1e798c7a95ed946f1038e6a92458a99d8f096
Vladislav <whatislov@mail.ru>
2025-05-09 00:46:59 +0300

    refactor: add dao and controller in service and register route for get all exchange rates operation

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,13 @@
 
 
 use App\CurrencyExchange\Controller\CurrencyController;
+use App\CurrencyExchange\Controller\ExchangeRateController;
 
 return [
     'GET' => [
         "#/currencies$#" => [CurrencyController::class, 'getAllCurrencies'],
         "#^/currency/(?P<currencyCode>[a-zA-Z]{3})$#" => [CurrencyController::class, 'getCurrency'],
+        "#/exchangeRates#" => [ExchangeRateController::class, 'getAllExchangeRates'],
     ],
     'POST' => [
         "#/currencies$#" => [CurrencyController::class, 'addCurrency'],
--- config/services.php
+++ config/services.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\CurrencyExchange\Controller\CurrencyController;
+use App\CurrencyExchange\Controller\ExchangeRateController;
 use App\CurrencyExchange\DAO\Currency\CurrencyDAOInterface;
 use App\CurrencyExchange\DAO\Currency\DBCurrencyDAO;
+use App\CurrencyExchange\DAO\ExchangeRate\DBExchangeRateDAO;
+use App\CurrencyExchange\DAO\ExchangeRate\ExchangeRateDAOInterface;
 use App\Framework\Container\Container;
 use App\Framework\Contract\AbstractRouter;
 use App\Framework\Router\HttpRouter;
@@ -26,9 +29,15 @@ return [
     CurrencyDAOInterface::class => function (Container $container) {
         return new DBCurrencyDAO($container->get(PDO::class));
     },
+    ExchangeRateDAOInterface::class => function (Container $container) {
+        return new DBExchangeRateDAO($container->get(PDO::class));
+    },
 
     # Controller
     CurrencyController::class => function (Container $container) {
         return new CurrencyController($container->get(CurrencyDAOInterface::class));
+    },
+    ExchangeRateController::class => function (Container $container) {
+        return new ExchangeRateController($container->get(ExchangeRateDAOInterface::class));
     }
 ];
