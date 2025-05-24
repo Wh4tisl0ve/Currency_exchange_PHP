3864aa03389ab8c7ac6a0bcc7190b65f156d3ae1
Vladislav <whatislov@mail.ru>
2025-05-25 00:19:39 +0300

    add all exchange rates methods and replace float type to BcMath\Number for rate

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS exchange_rates (
     id SERIAL PRIMARY KEY,
     base_currency_id INTEGER NOT NULL,
     target_currency_id INTEGER NOT NULL,
-    rate DECIMAL(6, 2) NOT NULL,
+    rate DECIMAL(11, 6) NOT NULL,
     UNIQUE(base_currency_id, target_currency_id),
     CHECK (base_currency_id <> target_currency_id),
     FOREIGN KEY (base_currency_id) REFERENCES currencies (id),
